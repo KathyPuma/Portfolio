2df786e4ed8d44faac19bca08ef4b7077a746996
KathyPuma <kathypuma96@gmail.com>
2020-06-27 23:29:26 -0400

    Setup front-end routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import About from './Components/About'
+import Navbar from './Components/Navbar'
+import Contact from './Components/Contact'
+import Project from './Components/Projects'
+import Skills from './Components/Skills'
+import './App.css'
 
 function App() {
+  
   return (
-    <div className="App">
+    <div>
+      <Navbar />
+
+      <Switch >
+        <Route path="/projects" component={Project} />
+        <Route path="/skills" component={Skills} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/" component={About} />
+      </Switch>
     </div>
   );
 }

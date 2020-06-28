461490f53ca7c5dc03f14d04cd87f009cc48b2bd
KathyPuma <kathypuma96@gmail.com>
2020-06-27 23:58:39 -0400

    Import Provider from react-redux and wrap the component around App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import themeStore from './redux/store/themeStore';
 import App from './App';
 
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-, document.getElementById('root'));
+    <Provider store={themeStore}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));

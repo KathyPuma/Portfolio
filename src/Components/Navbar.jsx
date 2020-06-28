c77abb4c54d5dbb10f76115953818b998f75cd1b
KathyPuma <kathypuma96@gmail.com>
2020-06-27 23:44:21 -0400

    Create a navbar with links, switch Navbar file from a class component to a functional component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import Resume from './KathyPuma_Resume.pdf'
+import './Navbar.css'
 
 
-class Navbar extends React.Component {
+function Navbar() {
 
+    return (
+        <div className='Navbar-stage'>
+            <a href={Resume} target="_blank" rel="noopener noreferrer" className='Nav-link'><h3 className='Nav-heading' >RESUME</h3></a>
+            <Link className='Nav-link' to="/"><h3 className='Nav-heading'>ABOUT</h3></Link>
+            <Link className='Nav-link' to="/projects"> <h3 className='Nav-heading'>PROJECTS</h3> </Link>{" "}
+            <Link className='Nav-link' to="/skills"> <h3 className='Nav-heading'>SKILLS</h3> </Link>{" "}
+            <Link className='Nav-link' to="/contact"> <h3 className='Nav-heading' >CONTACT</h3> </Link>{" "}
+        </div>
+    )
 
-    render() {
-        return (
-            <div className='Navbar-stage'>
-
-            </div>
-        )
-    }
 }
 
 export default Navbar;

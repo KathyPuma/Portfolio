caa1572e8d06ac44fe637a07f0cbe0f4f6ad0485
KathyPuma <kathypuma96@gmail.com>
2020-06-30 15:05:32 -0400

    Remove imported pdf and change the href url

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import Resume from './KathyPuma_Resume.pdf'
 import './Navbar.css'
 
 
@@ -8,7 +7,7 @@ function Navbar() {
 
     return (
         <div className='Navbar-stage'>
-            <a href={Resume} target="_blank" rel="noopener noreferrer" className='Nav-link'><h3 className='Nav-heading' >RESUME</h3></a>
+            <a href= 'https://docs.google.com/document/d/1B_LlHUV8gNFXVW_WE8ZNuYWC6WRj3QSw3rA7U6H3PYE/edit#heading=h.ytne192gwv0i' target="_blank" rel="noopener noreferrer" className='Nav-link'><h3 className='Nav-heading' >RESUME</h3></a>
             <Link className='Nav-link' to="/"><h3 className='Nav-heading'>ABOUT</h3></Link>
             <Link className='Nav-link' to="/projects"> <h3 className='Nav-heading'>PROJECTS</h3> </Link>{" "}
             <Link className='Nav-link' to="/skills"> <h3 className='Nav-heading'>SKILLS</h3> </Link>{" "}

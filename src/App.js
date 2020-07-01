a03ba5d5ed7bc3180759316593c6d9b5f4288c7d
KathyPuma <kathypuma96@gmail.com>
2020-06-30 20:20:52 -0400

    Pass mode as props to the Footer's page

--- src/App.js
+++ src/App.js
@@ -5,24 +5,26 @@ import Navbar from './Components/Navbar'
 import Contact from './Components/Contact'
 import Project from './Components/Projects'
 import Skills from './Components/Skills'
+import Footer from './Components/Footer'
 import { connect } from 'react-redux';
 import ToggleButton from './Components/ToggleButton'
-
 import './App.css'
 
 function App(props) {
 
   return (
     <div className={!props.mode ? "Light" : 'Dark'}>
-      <Navbar />
       <ToggleButton mode={props.mode} className='toggle' />
 
+      <Navbar />
       <Switch >
         <Route path="/projects" component={Project} />
         <Route path="/skills" component={Skills} />
         <Route path="/contact" component={Contact} />
         <Route path="/" component={About} />
       </Switch>
+      
+      <Footer lightMode={props.mode} />
     </div>
   );
 }

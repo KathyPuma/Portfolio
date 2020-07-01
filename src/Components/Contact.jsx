98846e837540d3c442ad7496adbbf64fbd8a6a32
KathyPuma <kathypuma96@gmail.com>
2020-06-30 21:11:32 -0400

    A network request is sent to formspree.io where if successful, the message will be sent to a specific email

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,34 +1,73 @@
-import React from 'react'
-
-import GithubLogo from './Icons/githubIcon.png'
-import LinkedinLogo from './Icons/LinkedinIcon.png'
+import React from "react";
 import './Contact.css'
+export default class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.submitForm = this.submitForm.bind(this);
+        this.state = {
+            status: ""
+        };
+    }
 
+    render() {
+        const { status } = this.state;
+        return (
+            <div className='Contact-stage'>
+                <form
+                    onSubmit={this.submitForm}
+                    action="https://formspree.io/moqknzno"
+                    method="POST"
+                    className='Contacfot-form'
+                >
 
-function Contact() {
-    return (
-        <div className='Contact-stage'>
-            <div className='Contact-container'>
-                <div className="Contact-icons">
+                    <label className='email-label' >Email:</label>
+                    <input className='input' type="email" name="email" />
+      
+                    <label className='message-label' >Message:</label>
+                    <input className='input' className = 'messageInput'type="text" name="message" />
 
-                    <div className='github-link'>
-                        <a href='https://github.com/KathyPuma' >
-                            <img className='icons' alt='github logo' src={GithubLogo} />
-                            Github
-                        </a>
-                    </div>
+                    {status === "SUCCESS" ? <p>Thanks!</p> : <button style={{
+                        backgroundColor: 'pink',
+                        color: 'white',
+                        padding: '12px 20px',
+                        border: 'none',
+                        borderRadius: '4px',
+                        cursor: 'pointer',
+                        float: 'right',
+                        display: 'flex',
+                        width: '100%',
+                        justifyContent: 'center',
+                        alignSelf: 'center',
+                        gridRowStart: 3,
+                        gridRowEnd: 3,
+                        gridColumnStart: 2,
+                        gridColumnEnd: 3,
+                        width: '25%',
+                        justifySelf: 'flex-end',
 
-                    <div className='linkedin-link'>
-                        <a href='https://www.linkedin.com/in/kathypuma/'>
-                            <img className='icons' alt='linkedin logo' src={LinkedinLogo} />Linkedin
-                        </a>
-                    </div>
-
-
-                </div>
+                    }}>Submit</button>}
+                    {status === "ERROR" && <p>Ooops! There was an error.</p>}
+                </form>
             </div>
-        </div>
-    )
-}
+        );
+    }
 
-export default Contact; 
+    submitForm(ev) {
+        ev.preventDefault();
+        const form = ev.target;
+        const data = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+                form.reset();
+                this.setState({ status: "SUCCESS" });
+            } else {
+                this.setState({ status: "ERROR" });
+            }
+        };
+        xhr.send(data);
+    }
+}

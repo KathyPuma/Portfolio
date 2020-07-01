316b028bfc0e85eb90f9211d48c5ed22a79262df
KathyPuma <kathypuma96@gmail.com>
2020-06-30 20:28:53 -0400

    Change href in a tag

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
 
     return (
         <div className='Navbar-stage'>
-            <a href= 'https://docs.google.com/document/d/1B_LlHUV8gNFXVW_WE8ZNuYWC6WRj3QSw3rA7U6H3PYE/edit#heading=h.ytne192gwv0i' target="_blank" rel="noopener noreferrer" className='Nav-link'><h3 className='Nav-heading' >RESUME</h3></a>
+            <a href='https://docs.google.com/document/d/1FeL5Xaz68vxATI7M20YxR8OmIuLsg-CQ1S3g2KOkQAk/edit#' target="_blank" rel="noopener noreferrer" className='Nav-link'><h3 className='Nav-heading' >RESUME</h3></a>
             <Link className='Nav-link' to="/"><h3 className='Nav-heading'>ABOUT</h3></Link>
             <Link className='Nav-link' to="/projects"> <h3 className='Nav-heading'>PROJECTS</h3> </Link>{" "}
             <Link className='Nav-link' to="/skills"> <h3 className='Nav-heading'>SKILLS</h3> </Link>{" "}

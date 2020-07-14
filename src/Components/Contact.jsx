f1f6f0b381bf92b8f87d934567fe420a916675c1
KathyPuma <kathypuma96@gmail.com>
2020-07-13 23:06:40 -0400

    Change Contact component from class to functional and change form to include an input for the sender's name

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,54 +1,23 @@
 import React from "react";
 import './Contact.css'
-export default class Contact extends React.Component {
-    constructor(props) {
-        super(props);
-        this.submitForm = this.submitForm.bind(this);
-        this.state = {
-            status: ""
-        };
-    }
 
-    render() {
-        const { status } = this.state;
-        return (
-            <div className='Contact-stage'>
-                <form
-                    onSubmit={this.submitForm}
-                    action="https://formspree.io/moqknzno"
-                    method="POST"
-                    className='Contact-form'
-                >
-                    <label className='email-label' >Email:</label>
-                    <input className='input' type="email" name="email" />
-
-                    <label className='message-label' >Message:</label>
-                    <input className='input' type="text" name="message" />
-
-                    {status === "SUCCESS" ? <p className= "submit-message">Email sent!</p> : 
-                    <button className = "submit-button" >Submit</button>}
-                    {status === "ERROR" && <p className= "submit-message">Message Failed to send</p>}
-                </form>
-            </div>
-        );
-    }
-
-    submitForm(ev) {
-        ev.preventDefault();
-        const form = ev.target;
-        const data = new FormData(form);
-        const xhr = new XMLHttpRequest();
-        xhr.open(form.method, form.action);
-        xhr.setRequestHeader("Accept", "application/json");
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState !== XMLHttpRequest.DONE) return;
-            if (xhr.status === 200) {
-                form.reset();
-                this.setState({ status: "SUCCESS" });
-            } else {
-                this.setState({ status: "ERROR" });
-            }
-        };
-        xhr.send(data);
-    }
+function Contact() {
+    return (
+        <div className='Contact-stage'>
+            <form className="contact-form" name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/kathypuma@pursuit.org" method="post">
+                <fieldset className="contact-form-inputs">
+                    <label htmlFor="name-label">Full Name</label>
+                    <input type="text" name="name" className="name-label" placeholder="First and Last Name" required></input>
+                    <label htmlFor="email-label">Email Address</label>
+                    <input type="email" name="_replyto" className="email-label" placeholder="email@domain.tld" required ></input>
+                    <label htmlFor="message-label">Message</label>
+                    <textarea rows="5" name="message" className="message-label" placeholder="Your message here..." required></textarea>
+                    <input type="hidden" name="_subject" className="email-subject" value="Contact Form Submission"></input>
+                </fieldset>
+                <input type="submit" value="Submit" className='submit-button'></input>
+            </form>
+        </div>
+    );
 }
+
+export default Contact

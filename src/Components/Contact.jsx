5bb746bc18840643187fbc8f659b60570c7e24b9
KathyPuma <kathypuma96@gmail.com>
2020-07-14 22:52:52 -0400

    Change label and placeholder for the contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,11 +6,11 @@ function Contact() {
         <div className='Contact-stage'>
             <form className="contact-form" name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/kathypuma@pursuit.org" method="post">
                 <fieldset className="contact-form-inputs">
-                    <label htmlFor="name-label">Full Name</label>
-                    <input type="text" name="name" className="name-label" placeholder="First and Last Name" required></input>
-                    <label htmlFor="email-label">Email Address</label>
+                    <label htmlFor="name-label">Name:</label>
+                    <input type="text" name="name" className="name-label" placeholder="Name" required></input>
+                    <label htmlFor="email-label">Email:</label>
                     <input type="email" name="_replyto" className="email-label" placeholder="email@domain.tld" required ></input>
-                    <label htmlFor="message-label">Message</label>
+                    <label htmlFor="message-label">Message:</label>
                     <textarea rows="5" name="message" className="message-label" placeholder="Your message here..." required></textarea>
                     <input type="hidden" name="_subject" className="email-subject" value="Contact Form Submission"></input>
                 </fieldset>

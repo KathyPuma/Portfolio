7d331a11ce9537bfc5f47bd38a08313533a35f98
KathyPuma <kathypuma96@gmail.com>
2020-07-18 00:01:40 -0400

    Rename classNames, update imported greenlist image  and update greenlist deployed url, and change class to className

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './Projects.css'
 import Carry from './Projects/Carry.png'
-import Greenlist from './Projects/Greenlist.png'
+import Greenlist from './Projects/Greenlist.jpg'
 import holDing from './Projects/holDing.png'
 
 function Projects() {
@@ -12,45 +12,45 @@ function Projects() {
     }, [])
 
     const handleAllProjects = () => {
-        let displayProject =
-            [
-                { name: 'Carry', about: 'A delivery platform for luxury clothes in NYC', img: Carry, github: 'https://github.com/KathyPuma/Carry', deployed: 'https://pursuitcarry.herokuapp.com/' },
-                { name: 'Greenlist', about: 'An registry that allows small designers and suppliers have a centralized source of finding materials.', img: Greenlist, github: 'https://github.com/KathyPuma/greenlist__FSWeb', deployed: 'https://github.com/' },
-                // { name: 'MovieList', about: 'A board where people can add shows and comments on it.', github: 'https://github.com/', deployed: 'https://github.com/' },
-                { name: 'holDing', about: 'A micro social media that allows user to join "holds". These holds are private with the purpose to create a safespace.', img: holDing, github: 'https://github.com/KathyPuma/holding__WebApp', deployed: 'https://github.com/' }
-            ]
-        setProjects(displayProject)
-    }
-
+                let displayProject =
+                    [
+                        { name: 'Carry', about: 'A delivery platform for luxury clothes in NYC', img: Carry, github: 'https://github.com/KathyPuma/Carry', deployed: 'https://pursuitcarry.herokuapp.com/' },
+                        { name: 'Greenlist', about: 'A registry that allows small designers and suppliers have a centralized source of finding materials.', img: Greenlist, github: 'https://github.com/KathyPuma/greenlist__FSWeb', deployed: 'https://greenlist-registry.herokuapp.com/main' },
+                        // { name: 'holDing', about: 'A micro social media that allows user to join "holds". These holds are private with the purpose to create a safespace.', img: holDing, github: 'https://github.com/KathyPuma/holding__WebApp' },
+                    ]
+                setProjects(displayProject)
+            }
 
     return (
-        <div class="main">
-            <ul class="cards">
+        <div className="Projects-stage">
+            <ul className="cards">
                 {projects.map(project => {
                     return (
-                        <li class="cards_item">
-                            <div class="card">
-                                <div class="card_image"><img src={project.img} alt={project.name} /></div>
-                                <div class="card_content">
-                                    {/* <h2 class="card_title">TITLE</h2>
-                                                <p class="card_text">INFO</p> */}
-                                    <button class="btn card_btn" >
-                                        <a
-                                            target='_blank'
-                                            href={project.deployed}
-                                            className='website'
-                                            rel='noopener noreferrer'
-                                        >WEBSITE</a>
-                                    </button>
-                                    <button class="btn card_btn" >
-                                        <a
-                                            target='_blank'
-                                            href={project.github}
-                                            className='website'
-                                            rel='noopener noreferrer'>
-                                            Github</a>
+                        <li className="cards_item">
+                            <div className="card">
+                                <img src={project.img} alt="Avatar" className="project_image" />
+                                <div className="overlay">
+                                    <div className="card_text">{project.about}</div>
 
-                                    </button>
+                                    <div className="card_content">
+                                        <button className="card_btn" >
+                                            <a
+                                                target='_blank'
+                                                href={project.deployed}
+                                                className='website'
+                                                rel='noopener noreferrer'
+                                            >WEBSITE</a>
+                                        </button>
+                                        <button className="card_btn" >
+                                            <a
+                                                target='_blank'
+                                                href={project.github}
+                                                className='website'
+                                                rel='noopener noreferrer'>
+                                                Github</a>
+
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </li>
@@ -58,7 +58,6 @@ function Projects() {
                 })
                 }
             </ul>
-
         </div>
     )
 }

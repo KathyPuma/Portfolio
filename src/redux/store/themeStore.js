e89bbb4f5925e1b9bf324b5af1498d7befb92325
KathyPuma <kathypuma96@gmail.com>
2020-07-21 21:33:53 -0400

    Add local storage functions to make sure state saves and persists

--- src/redux/store/themeStore.js
+++ src/redux/store/themeStore.js
@@ -1,10 +1,38 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 
+
+const saveToLocalStorage = (themeStore) => {
+  try {
+    const serializedState = JSON.stringify(themeStore)
+    localStorage.setItem('themeStore', serializedState)
+  } catch (err) {
+    console.log("ERROR", err)
+  }
+}
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem('themeStore')
+    if (serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch (err) {
+    console.log("ERROR", err)
+    return undefined
+  }
+}
+
+const persistedState = loadFromLocalStorage()
+
 const themeStore = createStore(
   rootReducer,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+themeStore.subscribe(() => saveToLocalStorage(themeStore.getState()))
 
 
 export default themeStore;

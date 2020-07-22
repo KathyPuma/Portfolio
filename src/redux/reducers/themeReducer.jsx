1aef8c6262d59c53e88214fbe438374ad0e39df1
KathyPuma <kathypuma96@gmail.com>
2020-07-21 21:30:41 -0400

    Add toggleChecked to state which controls if toggle button is checked

--- src/redux/reducers/themeReducer.jsx
+++ src/redux/reducers/themeReducer.jsx
@@ -1,7 +1,7 @@
-export default function rootReducer(state = { darkMode: false }, action) {
+export default function rootReducer(state = { darkMode: false, toggleChecked: false }, action) {
    switch (action.type) {
       case "CHANGE_MODE":
-         return { ...state, darkMode: !state.darkMode };
+         return { ...state, darkMode: !state.darkMode, toggleChecked: !state.toggleChecked };
       default:
          return state
    }

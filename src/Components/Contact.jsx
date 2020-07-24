6e5da4de79208affc40b63d4850bbbc644bc6296
KathyPuma <kathypuma96@gmail.com>
2020-07-24 03:30:46 -0400

    Add conditional to className to take account for darkMode

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Footer from './Footer'
+import themeStore from '../redux/store/themeStore'
 import './Contact.css'
 
 function Contact() {
+    const state = themeStore.getState();
     return (
         <div className='Contact-stage'>
             <form className="contact-form" name="simple-contact-form" acceptCharset="utf-8" action="https://formspree.io/kathypuma@pursuit.org" method="post">
@@ -15,7 +17,9 @@ function Contact() {
                     <textarea rows="5" name="message" className="message-label" placeholder="Your message here..." required></textarea>
                     <input type="hidden" name="_subject" className="email-subject" value="Contact Form Submission"></input>
                 </fieldset>
-                <input type="submit" value="Submit" className='submit-button'></input>
+                <input type="submit" value="Submit" 
+                className={state.theme.darkMode ? "submit-button-lightMode" : "submit-button-darkMode"}>
+                </input>
             </form>
             
             <Footer/>

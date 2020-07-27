b9c41813bd94a89113e3c14ad03a92ee49b5302f
KathyPuma <kathypuma96@gmail.com>
2020-07-27 13:55:40 -0400

    Import image and put image in a img tag

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Footer from './Footer'
+import KathyPuma from './Projects/KathyPuma.jpg'
 import './About.css'
 
 
@@ -7,7 +8,8 @@ function About() {
     return (
         <div className='About-stage'>
             <div className='About-container'>
-                <div className='About-card'>
+            <img src = {KathyPuma} alt='profile-photo' className ='profile-photo'/>
+                <div className='About-card'>                   
                     <p className='About-description'>
                         Graduated with a bachelor's degree in Accounting and Economics but grew a passion for tech in college after taking a Computer Science course.
                         Deciding it was too late to change majors, I continued on and graduated but joined Pursuit right after where I learn PERN stack.

ad1515755ed77fd577589df5d27d61436c268145
KathyPuma <kathypuma96@gmail.com>
2020-09-01 21:52:45 -0400

    Wrap Links into divs

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,20 +9,36 @@ function Navbar() {
 
     return (
         <div className='Navbar-stage'>
-            <a href='https://docs.google.com/document/d/1FeL5Xaz68vxATI7M20YxR8OmIuLsg-CQ1S3g2KOkQAk/edit#' target="_blank" rel="noopener noreferrer" className='Nav-link'>
-                <h3 className={state.theme.darkMode ? "Nav-heading-lightMode" : "Nav-heading-darkMode"}  >RESUME</h3></a>
-            <Link className={state.theme.darkMode ? "Nav-link-lightMode" : "Nav-link-darkMode"} to="/">
-                <h3 className={state.theme.darkMode ? "Nav-heading-lightMode" : "Nav-heading-darkMode"}  >ABOUT</h3>
-            </Link>
-            <Link className={state.theme.darkMode ? "Nav-link-lightMode" : "Nav-link-darkMode"} to="/projects">
-                <h3 className={state.theme.darkMode ? "Nav-heading-lightMode" : "Nav-heading-darkMode"}>PROJECTS</h3>
-            </Link>{" "}
-            <Link className={state.theme.darkMode ? "Nav-link-lightMode" : "Nav-link-darkMode"} to="/skills">
-                <h3 className={state.theme.darkMode ? "Nav-heading-lightMode" : "Nav-heading-darkMode"}>SKILLS</h3>
-            </Link>{" "}
-            <Link className={state.theme.darkMode ? "Nav-link-lightMode" : "Nav-link-darkMode"} to="/contact">
-                <h3 className={state.theme.darkMode ? "Nav-heading-lightMode" : "Nav-heading-darkMode"} >CONTACT</h3>
-            </Link>{" "}
+
+            <div className='link-div'>
+                <a href='https://docs.google.com/document/d/1FeL5Xaz68vxATI7M20YxR8OmIuLsg-CQ1S3g2KOkQAk/edit#' target="_blank" rel="noopener noreferrer" className='Nav-link'>
+                    <h3 className={state.theme.darkMode ? "Nav-heading-lightMode" : "Nav-heading-darkMode"}  >RESUME</h3>
+                </a>
+            </div>
+            
+            <div className='link-div'>
+                <Link className={state.theme.darkMode ? "Nav-link-lightMode" : "Nav-link-darkMode"} to="/">
+                    <h3 className={state.theme.darkMode ? "Nav-heading-lightMode" : "Nav-heading-darkMode"}  >ABOUT</h3>
+                </Link>
+            </div>
+
+            <div className='link-div'>
+                <Link className={state.theme.darkMode ? "Nav-link-lightMode" : "Nav-link-darkMode"} to="/projects">
+                    <h3 className={state.theme.darkMode ? "Nav-heading-lightMode" : "Nav-heading-darkMode"}>PROJECTS</h3>
+                </Link>{" "}
+            </div>
+
+            <div className='link-div'>
+                <Link className={state.theme.darkMode ? "Nav-link-lightMode" : "Nav-link-darkMode"} to="/skills">
+                    <h3 className={state.theme.darkMode ? "Nav-heading-lightMode" : "Nav-heading-darkMode"}>SKILLS</h3>
+                </Link>{" "}
+            </div>
+
+            <div className='link-div'>
+                <Link className={state.theme.darkMode ? "Nav-link-lightMode" : "Nav-link-darkMode"} to="/contact">
+                    <h3 className={state.theme.darkMode ? "Nav-heading-lightMode" : "Nav-heading-darkMode"} >CONTACT</h3>
+                </Link>{" "}
+            </div>
         </div>
     )
 }

1cd634d2bc0979952a4dd5f15ed3e18f3be7ca56
KathyPuma <kathypuma96@gmail.com>
2020-09-10 22:45:12 -0400

    Add deployed link to reddit clone

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -18,7 +18,7 @@ function Projects() {
     const handleAllProjects = () => {
                 let displayProject =
                     [
-                        { name: 'RedditClone', about: 'A clone that mimics reddit.', img: RedditClone, github: 'https://github.com/KathyPuma/Reddit_clone', deployed: '' },
+                        { name: 'RedditClone', about: 'A clone that mimics reddit.', img: RedditClone, github: 'https://github.com/KathyPuma/Reddit_clone', deployed: 'http://reddit-replica1.herokuapp.com/' },
                         { name: 'Carry', about: 'A delivery platform for luxury clothes in NYC.', img: Carry, github: 'https://github.com/KathyPuma/Carry', deployed: 'https://pursuitcarry.herokuapp.com/' },
                         { name: 'MovieMarathon', about: 'A movie/show review hub that allows users to see what others are watching and recommending.', img: MovieMarathon, github: 'https://github.com/KathyPuma/MovieMarathon', deployed: 'https://moivemarathon.herokuapp.com/' },
                         { name: 'Greenlist', about: 'A registry that allows small designers and suppliers have a centralized source of finding materials.', img: Greenlist, github: 'https://github.com/KathyPuma/greenlist__FSWeb', deployed: 'https://greenlist-registry.herokuapp.com/main' },
